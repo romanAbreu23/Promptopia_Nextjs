c303245e75408ef2b7d1ce3a623c2640fa2575fb
Roman Abreu <abreulr19@gmail.com>
2024-06-06 13:50:50 -0600

    first typo

--- components/UpdatePrompt.jsx
+++ components/UpdatePrompt.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -63,3 +65,5 @@ const EditPrompt = () => {
         />
     )
 }
+
+export default EditPrompt;
